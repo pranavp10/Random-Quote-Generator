e351d075f3882a6cca826eb47d5265b288caeb80
Pranav P <pranavkp.me@outlook.com>
2020-04-06 11:38:16 +0530

    Updates

---
+++ precache-manifest.a69062995b285e702545030f412453ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aad230afa1e2156fb657ffc3aaac21d",
+    "url": "/Random-Quote-Generator/index.html"
+  },
+  {
+    "revision": "c71c9daba3d2e1be1b97",
+    "url": "/Random-Quote-Generator/static/css/main.06aad084.chunk.css"
+  },
+  {
+    "revision": "3da8ab624fc0c574ac5f",
+    "url": "/Random-Quote-Generator/static/js/2.9354a712.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Random-Quote-Generator/static/js/2.9354a712.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c71c9daba3d2e1be1b97",
+    "url": "/Random-Quote-Generator/static/js/main.fb152633.chunk.js"
+  },
+  {
+    "revision": "713e9f23c4bc0ad40fcc",
+    "url": "/Random-Quote-Generator/static/js/runtime-main.3455ee10.js"
+  }
+]);
